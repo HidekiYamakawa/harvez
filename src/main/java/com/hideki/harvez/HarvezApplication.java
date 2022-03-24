c5411a1e52eaa6e334db064e5f42467bab49775c
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2022-03-23 23:15:42 -0300

    Project created

---
+++ src/main/java/com/hideki/harvez/HarvezApplication.java
@@ -0,0 +1,13 @@
+package com.hideki.harvez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hideki/harvez/HarvezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hideki.harvez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
