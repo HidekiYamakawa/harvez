e264887f5b49a6ea3ced655dbcc5ab0a62ae8d1a
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2022-05-24 17:59:50 -0300

    Inconsistencies fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
-INSERT INTO user (id, name, email, password) VALUES (1, 'Admin', 'admin@email.com', '$2a$10$TjiNqZ7koeqHWe7u1bkdA.6MYTcP5A7U0Yxdt.gw90JDM/KX5IWsy');
-INSERT INTO user (id, name, email, password) VALUES (2, 'Caminhoneiro', 'caminhoneiro@email.com', '$2a$10$TjiNqZ7koeqHWe7u1bkdA.6MYTcP5A7U0Yxdt.gw90JDM/KX5IWsy');
+INSERT INTO user (name, email, password) VALUES ('Admin', 'admin@email.com', '$2a$10$TjiNqZ7koeqHWe7u1bkdA.6MYTcP5A7U0Yxdt.gw90JDM/KX5IWsy');
+INSERT INTO user (name, email, password) VALUES ('Caminhoneiro', 'caminhoneiro@email.com', '$2a$10$TjiNqZ7koeqHWe7u1bkdA.6MYTcP5A7U0Yxdt.gw90JDM/KX5IWsy');
 
-INSERT INTO role (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO role (id, name) VALUES (2, 'ROLE_CAMINHONEIRO');
+INSERT INTO role (name) VALUES ('ROLE_ADMIN');
+INSERT INTO role (name) VALUES ('ROLE_CAMINHONEIRO');
 
 INSERT INTO user_roles (user_id, roles_id) VALUES (1, 1);
 INSERT INTO user_roles (user_id, roles_id) VALUES (2, 2);
 
-INSERT INTO employee (id, name, office) VALUES (1, 'Carlos Hideki Yamakawa', 'Gerente');
-INSERT INTO employee (id, name, office) VALUES (2, 'Natalia Yumi Nakamura', 'Líder');
-INSERT INTO employee (id, name, office) VALUES (3,'Eliana Mayumi Imanisse Yamakawa', 'Adminstradora');
-INSERT INTO employee (id, name, office) VALUES (4,'Yukio Yamakawa', 'Presidente');
-INSERT INTO employee (id, name, office) VALUES (5, 'Leticia Satie Yamakawa', 'Vendedora');
-INSERT INTO employee (id, name, office) VALUES (6, 'Alberto Yugi Yamakawa', 'Agricultor');
+INSERT INTO employee (name, office) VALUES ('Carlos Hideki Yamakawa', 'Gerente');
+INSERT INTO employee (name, office) VALUES ('Natalia Yumi Nakamura', 'Líder');
+INSERT INTO employee (name, office) VALUES ('Eliana Mayumi Imanisse Yamakawa', 'Adminstradora');
+INSERT INTO employee (name, office) VALUES ('Yukio Yamakawa', 'Presidente');
+INSERT INTO employee (name, office) VALUES ('Leticia Satie Yamakawa', 'Vendedora');
+INSERT INTO employee (name, office) VALUES ('Alberto Yugi Yamakawa', 'Agricultor');
--- src/test/java/com/hideki/harvez/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/hideki/harvez/repository/EmployeeRepositoryTest.java
@@ -1,8 +1,8 @@
 package com.hideki.harvez.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
