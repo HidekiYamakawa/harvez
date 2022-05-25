af85bef6c87815985ac6e79d3be3dc11fd3d1913
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2022-05-25 12:54:47 -0300

    Package changed from war to jar (revert)

--- src/main/java/com/hideki/harvez/HarvezApplication.java
+++ src/main/java/com/hideki/harvez/HarvezApplication.java
@@ -2,22 +2,15 @@ package com.hideki.harvez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
-public class HarvezApplication extends SpringBootServletInitializer {
+public class HarvezApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HarvezApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(HarvezApplication.class);
 	}
 }
