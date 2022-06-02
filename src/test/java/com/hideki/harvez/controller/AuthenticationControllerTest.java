7bc9d6d322d79a0c6b7a20fa59e5ec43256b0953
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2022-06-02 14:39:22 -0300

    Changed dev database from h2 to MySql

--- src/test/java/com/hideki/harvez/controller/AuthenticationControllerTest.java
+++ src/test/java/com/hideki/harvez/controller/AuthenticationControllerTest.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class AuthenticationControllerTest {
 
